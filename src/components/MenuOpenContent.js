0be4a96177756801362f74550a9f7cd18542158f
marulita18 <crespo.mariel@gmail.com>
2021-12-01 15:46:52 +0100

    added caroussel images

---
+++ src/components/MenuOpenContent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ImageCaroussel from "./ImageCaroussel";
+
+export default function MenuOpenContent() {
+  return (
+    <div>
+      <ImageCaroussel />
+    </div>
+  );
+}
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import Menu from "../components/Menu";
+import MenuOpenContent from "../components/MenuOpenContent";
 
 export default function Homepage() {
   const [openMenu, setOpenMenu] = useState(false);
@@ -9,7 +10,7 @@ export default function Homepage() {
     <div>
       <Menu openMenu={openMenu} setOpenMenu={setOpenMenu} />
 
-      {openMenu ? "display this" : "display nothing"}
+      {openMenu ? <MenuOpenContent /> : "display nothing"}
     </div>
   );
 }
